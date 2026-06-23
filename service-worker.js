07a58ac231c892213d0a0ca5632abbab36479e9a
juliendamiani-cmd <julien.damiani@gmail.com>
2026-06-23 07:36:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,17 @@
-const CACHE_NAME = "trail-tracker-v10";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icon.svg"
-];
+const CACHE_NAME = "trail-tracker-v30";
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
